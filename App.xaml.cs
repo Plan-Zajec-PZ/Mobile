942ce10537974dc258309bc1f6493ac2a84e4f2c
Mariusz Skuza <marsku8@wp.pl>
2022-10-17 21:38:14 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiCalendarApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiCalendarApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
